41fc34bf700ad6f5c901280dcd3d775f8a5b8591
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-05-27 01:44:36 -0400

    Update header.blade.php

--- resources/views/frontend/account/components/header.blade.php
+++ resources/views/frontend/account/components/header.blade.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" href="styles.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="{{ asset('css/font-awesome-4.7.0/css/font-awesome.css') }}">
         <link rel="stylesheet" href="{{ asset('css/login.css') }}">
         <link rel="stylesheet" href="{{ asset('css/reset-password.css') }}">
     </head>
