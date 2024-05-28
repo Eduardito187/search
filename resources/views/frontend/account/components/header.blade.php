83f5433c5789499a9a7c2d559dc1a7734328ac8a
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-05-28 01:55:05 -0400

    vue

--- resources/views/frontend/account/components/header.blade.php
+++ resources/views/frontend/account/components/header.blade.php
@@ -2,13 +2,12 @@
 <html lang="es">
     <head>
         <title>@yield('title')</title>
-        <link rel="stylesheet" href="styles.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="{{ asset('css/font-awesome-4.7.0/css/font-awesome.css') }}">
         <link rel="stylesheet" href="{{ asset('css/login.css') }}">
         <link rel="stylesheet" href="{{ asset('css/reset-password.css') }}">
-        <script src="{{ asset('js/vue.js') }}"></script>
+        <script src="{{ asset('js/Vue/vue.js') }}"></script>
         @yield('custom-header')
     </head>
     <body class="{{ $bodyClass }}">
