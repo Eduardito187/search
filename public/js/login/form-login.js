b6fed4aaa02bb900f4536066c82adf26372038f5
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-05-29 01:24:09 -0400

    Update form-login.js

--- public/js/login/form-login.js
+++ public/js/login/form-login.js
@@ -19,30 +19,6 @@ new Vue({
             }
 
             $.ajax({
-                url: window.configFrontend.base_url_frontend+'api/account/generate-password',
-                type: 'POST',
-                data: JSON.stringify(
-                    {
-                        password:this.password
-                    }
-                ),
-                contentType: 'application/json',
-                dataType: 'json',
-                showLoader: true,
-                headers: {
-                    'Authorization': "Bearer "+window.configFrontend.token_access_frontend,
-                    'Cache-Control': 'no-cache'
-                },
-                success: function (response) {
-                    console.log(response);
-                },
-                error: function (error) {
-                    this.classMessageInfo = 'alert-danger';
-                    this.messageInfo = error;
-                }
-            });
-
-            $.ajax({
                 url: window.configFrontend.base_url_frontend+'api/account/login',
                 type: 'POST',
                 data: JSON.stringify(
