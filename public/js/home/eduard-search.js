f620ea21a1f56827d9ae1465f8a81b34befce92f
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-03 01:22:53 -0400

    Update eduard-search.js

--- public/js/home/eduard-search.js
+++ public/js/home/eduard-search.js
@@ -125,7 +125,7 @@ new Vue({
             var options = {
               series: [44, 55, 13, 43, 22],
               chart: {
-              width: 380,
+              width: '100%',
               type: 'pie',
             },
             labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
