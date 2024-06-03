5175827774bb305d5d1a912237680d5025587f06
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-03 00:06:20 -0400

    Update home.blade.php

--- resources/views/frontend/account/home/home.blade.php
+++ resources/views/frontend/account/home/home.blade.php
@@ -4,6 +4,7 @@
 
 @section('custom-header')
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <script src="{{ asset('js/home/eduard-search.js') }}"></script>
 @endsection
 
 @section('content')
@@ -53,5 +54,4 @@
 @endsection
 
 @section('custom-footer')
-    <script src="{{ asset('js/home/eduard-search.js') }}"></script>
 @endsection
