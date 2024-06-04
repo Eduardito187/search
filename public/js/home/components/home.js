e823cf451781888abc982c4118aa8639219712c4
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-04 01:41:57 -0400

    Update home.js

--- public/js/home/components/home.js
+++ public/js/home/components/home.js
@@ -1,43 +1,45 @@
 var HomeSection = {
     template: `
-    <div class="jumbotron">
-        <h1 class="display-4">Welcome!</h1>
-        <p class="lead">Check what's happening on your EduardSearch implementation.</p>
-        <hr class="my-4">
-    </div>
-    <div class="card">
-        <div class="card-header">
-            Busquedas
+    <div>
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome!</h1>
+            <p class="lead">Check what's happening on your EduardSearch implementation.</p>
+            <hr class="my-4">
         </div>
-        <div class="card-body">
-            <blockquote class="blockquote mb-0">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-            </blockquote>
+        <div class="card">
+            <div class="card-header">
+                Busquedas
+            </div>
+            <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                </blockquote>
+            </div>
         </div>
-    </div>
 
-    <div class="card">
-        <div class="card-header">
-            Sugeridos
+        <div class="card">
+            <div class="card-header">
+                Sugeridos
+            </div>
+            <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                </blockquote>
+            </div>
         </div>
-        <div class="card-body">
-            <blockquote class="blockquote mb-0">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-            </blockquote>
-        </div>
-    </div>
-    
-    <div class="card">
-        <div class="card-header">
-            Data
-        </div>
-        <div class="card-body">
-            <blockquote class="blockquote mb-0">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-            </blockquote>
+
+        <div class="card">
+            <div class="card-header">
+                Data
+            </div>
+            <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                </blockquote>
+            </div>
         </div>
     </div>
     `
