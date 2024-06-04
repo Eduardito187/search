7a2e4e8b2873d888ecebb218c14ecf527c3db9c1
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-03 22:08:02 -0400

    vue var

--- public/js/home/eduard-search.js
+++ public/js/home/eduard-search.js
@@ -41,15 +41,17 @@ const router = new VueRouter({
 Vue.prototype.$versionApp = window.configFrontend.version_frontend;
 Vue.prototype.$appName = window.configFrontend.app_name_frontend;
 Vue.prototype.$currentYear = window.configFrontend.server_year;
-Vue.prototype.$loadedPage = false;
-Vue.prototype.$customer = null;
 
 new Vue({
   el: '#home-container',
   router,
+  data: {
+    customer: null,
+    loadedPage: false,
+  },
   methods: {
     loadedCustomer() {
-      let self = Vue.prototype;
+      let self = this;
 
       fetch(window.configFrontend.base_url_frontend + 'api/account/customer-information', {
         method: 'POST',
@@ -63,8 +65,8 @@ new Vue({
         .then(response => response.json())
         .then(data => {
           if (data.status && data.code == 200) {
-            self.$customer = data.response;
-            self.$loadedPage = true;
+            self.customer = data.response;
+            self.loadedPage = true;
             console.log(data);
           }
         })
