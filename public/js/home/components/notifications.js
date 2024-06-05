fbd1bed409d2938397a9e9207c0fe1af7cf9a5b8
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-05 01:45:20 -0400

    Update notifications.js

--- public/js/home/components/notifications.js
+++ public/js/home/components/notifications.js
@@ -3,52 +3,60 @@ var NotificationsSection = {
     <div class="row">
         <div class="col-md-12">
             <div class="card">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div>
-                        <h5 class="card-title">Weekly summary reports</h5>
-                        <p class="card-text">Algolia sends weekly reports to keep you updated on the search activity and usage of your users and other such analytics. Recommended if you are interested in the performance of your search.</p>
-                    </div>
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" role="switch" id="weeklySummaryReports">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-12">
-            <div class="card">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div>
-                        <h5 class="card-title">Usage alerts</h5>
-                        <p class="card-text">Receive notifications when your application(s) reach 50%, 80% and 100% of your Free or Committed plan's usage quota.</p>
-                    </div>
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" role="switch" id="usageAlerts">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div>
+                            <h5 class="card-title">Weekly summary reports</h5>
+                            <p class="card-text">Algolia sends weekly reports to keep you updated on the search activity and usage of your users and other such analytics. Recommended if you are interested in the performance of your search.</p>
+                        </div>
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="weeklySummaryReports">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-md-12">
             <div class="card">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div>
-                        <h5 class="card-title">Billing</h5>
-                        <p class="card-text">Receive invoices and notifications about payment/credit card issues. Recommended for application owners and people in charge of billing and invoicing.</p>
-                    </div>
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" role="switch" id="billing">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div>
+                            <h5 class="card-title">Usage alerts</h5>
+                            <p class="card-text">Receive notifications when your application(s) reach 50%, 80% and 100% of your Free or Committed plan's usage quota.</p>
+                        </div>
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="usageAlerts">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-md-12">
             <div class="card">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div>
-                        <h5 class="card-title">AI</h5>
-                        <p class="card-text">Algolia sends weekly notifications to keep you updated about your AI features activity. Recommended if you are interested in the impact of AI on your search performance.</p>
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div>
+                            <h5 class="card-title">Billing</h5>
+                            <p class="card-text">Receive invoices and notifications about payment/credit card issues. Recommended for application owners and people in charge of billing and invoicing.</p>
+                        </div>
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="billing">
+                        </div>
                     </div>
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" role="switch" id="ai">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div>
+                            <h5 class="card-title">AI</h5>
+                            <p class="card-text">Algolia sends weekly notifications to keep you updated about your AI features activity. Recommended if you are interested in the impact of AI on your search performance.</p>
+                        </div>
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="ai">
+                        </div>
                     </div>
                 </div>
             </div>
