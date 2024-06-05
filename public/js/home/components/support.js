6839455b1d54c86b60829618ef5a3928d979ffbe
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-05 01:30:56 -0400

    support

--- public/js/home/components/support.js
+++ public/js/home/components/support.js
@@ -1,38 +1,43 @@
 var SupportSection = {
     template: `
     <div class="row">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-4">
-                        <h5 class="card-title">Algolia Support Access</h5>
-                        <p class="card-text">In order to investigate a bug or improve relevance, it is sometimes useful to grant Algolia support access to the content of your indices.</p>
-                        <div class="alert alert-info custom-alert" role="alert">
-                            <i class="bi bi-info-circle"></i> Algolia cannot currently access your account.
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h5 class="card-title">Algolia Support Access</h5>
+                            <p class="card-text">In order to investigate a bug or improve relevance, it is sometimes useful to grant Algolia support access to the content of your indices.</p>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="row">
+                                <div class="alert alert-info custom-alert" role="alert">
+                                    <i class="bi bi-info-circle"></i> Algolia cannot currently access your account.
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label for="access-level" class="form-label">Access level</label>
+                                    <select class="form-select" id="access-level">
+                                        <option value="read">read</option>
+                                        <option value="write">write</option>
+                                        <option value="admin">admin</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="period" class="form-label">Period</label>
+                                    <select class="form-select" id="period">
+                                        <option value="7">7 days</option>
+                                        <option value="14">14 days</option>
+                                        <option value="30">30 days</option>
+                                    </select>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-8">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label for="access-level" class="form-label">Access level</label>
-                                <select class="form-select custom-select" id="access-level">
-                                    <option value="read">read</option>
-                                    <option value="write">write</option>
-                                    <option value="admin">admin</option>
-                                </select>
-                            </div>
-                            <div class="col-md-6">
-                                <label for="period" class="form-label">Period</label>
-                                <select class="form-select custom-select" id="period">
-                                    <option value="7">7 days</option>
-                                    <option value="14">14 days</option>
-                                    <option value="30">30 days</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <button type="submit" class="btn btn-primary mt-3">Save</button>
-                        </div>
+                    <hr>
+                    <div class="row">
+                        <button type="submit" class="btn btn-primary mt-3">Save</button>
                     </div>
                 </div>
             </div>
