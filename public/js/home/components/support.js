2df35ed4126034207ebcf8ac0b7aeb7f78e54ec0
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-05 01:28:35 -0400

    Update support.js

--- public/js/home/components/support.js
+++ public/js/home/components/support.js
@@ -1,42 +1,37 @@
 var SupportSection = {
     template: `
-    <div class="container">
-        <div class="card custom-card">
+    <div class="row">
+        <div class="card">
             <div class="card-body">
-                <h5 class="card-title">Support</h5>
-                <div class="card">
-                    <div class="card-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h5 class="card-title">Algolia Support Access</h5>
+                        <p class="card-text">In order to investigate a bug or improve relevance, it is sometimes useful to grant Algolia support access to the content of your indices.</p>
+                        <div class="alert alert-info custom-alert" role="alert">
+                            <i class="bi bi-info-circle"></i> Algolia cannot currently access your account.
+                        </div>
+                    </div>
+                    <div class="col-md-8">
                         <div class="row">
-                            <div class="col-md-4">
-                                <h5 class="card-title">Algolia Support Access</h5>
-                                <p class="card-text">In order to investigate a bug or improve relevance, it is sometimes useful to grant Algolia support access to the content of your indices.</p>
-                                <div class="alert alert-info custom-alert" role="alert">
-                                    <i class="bi bi-info-circle"></i> Algolia cannot currently access your account.
-                                </div>
+                            <div class="col-md-6">
+                                <label for="access-level" class="form-label">Access level</label>
+                                <select class="form-select custom-select" id="access-level">
+                                    <option value="read">read</option>
+                                    <option value="write">write</option>
+                                    <option value="admin">admin</option>
+                                </select>
                             </div>
-                            <div class="col-md-8">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <label for="access-level" class="form-label">Access level</label>
-                                        <select class="form-select custom-select" id="access-level">
-                                            <option value="read">read</option>
-                                            <option value="write">write</option>
-                                            <option value="admin">admin</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="period" class="form-label">Period</label>
-                                        <select class="form-select custom-select" id="period">
-                                            <option value="7">7 days</option>
-                                            <option value="14">14 days</option>
-                                            <option value="30">30 days</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <button type="submit" class="btn btn-primary mt-3">Save</button>
-                                </div>
+                            <div class="col-md-6">
+                                <label for="period" class="form-label">Period</label>
+                                <select class="form-select custom-select" id="period">
+                                    <option value="7">7 days</option>
+                                    <option value="14">14 days</option>
+                                    <option value="30">30 days</option>
+                                </select>
                             </div>
+                        </div>
+                        <div class="row">
+                            <button type="submit" class="btn btn-primary mt-3">Save</button>
                         </div>
                     </div>
                 </div>
