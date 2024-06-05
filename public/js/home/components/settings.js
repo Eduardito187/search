2ea3ad082e1ad229600de0c5615507b69ae86e33
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-05 01:05:05 -0400

    Update settings.js

--- public/js/home/components/settings.js
+++ public/js/home/components/settings.js
@@ -3,58 +3,66 @@ var SettingsSection = {
     <div class="container mt-5">
         <div class="row">
             <h2>Organization Settings</h2>
-            <div class="col-md-12">
-                <div class="card col-md-4 mb-3">
-                    <div class="card-header">
-                        General
-                    </div>
-                    <div class="card-body">
-                        <div class="list-group">
-                            <a href="#" class="list-group-item list-group-item-action">Applications</a>
-                            <a href="#" class="list-group-item list-group-item-action">Infrastructure</a>
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-header">
+                            General
+                        </div>
+                        <div class="card-body">
+                            <div class="list-group">
+                                <a href="#" class="list-group-item list-group-item-action">Applications</a>
+                                <a href="#" class="list-group-item list-group-item-action">Infrastructure</a>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="card col-md-4 mb-3">
-                    <div class="card-header">
-                        Billing
-                    </div>
-                    <div class="card-body">
-                        <div class="list-group">
-                            <a href="#" class="list-group-item list-group-item-action">Your plan and billing</a>
-                            <a href="#" class="list-group-item list-group-item-action">Usage</a>
-                            <a href="#" class="list-group-item list-group-item-action">Cost Management</a>
-                            <a href="#" class="list-group-item list-group-item-action">Payment Details</a>
-                            <a href="#" class="list-group-item list-group-item-action">Invoices</a>
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Billing
+                        </div>
+                        <div class="card-body">
+                            <div class="list-group">
+                                <a href="#" class="list-group-item list-group-item-action">Your plan and billing</a>
+                                <a href="#" class="list-group-item list-group-item-action">Usage</a>
+                                <a href="#" class="list-group-item list-group-item-action">Cost Management</a>
+                                <a href="#" class="list-group-item list-group-item-action">Payment Details</a>
+                                <a href="#" class="list-group-item list-group-item-action">Invoices</a>
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="card col-md-4 mb-3">
-                    <div class="card-header">
-                        Team and Access
-                    </div>
-                    <div class="card-body">
-                        <div class="list-group">
-                            <a href="#" class="list-group-item list-group-item-action">API Keys</a>
-                            <a href="#" class="list-group-item list-group-item-action">Team</a>
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Team and Access
+                        </div>
+                        <div class="card-body">
+                            <div class="list-group">
+                                <a href="#" class="list-group-item list-group-item-action">API Keys</a>
+                                <a href="#" class="list-group-item list-group-item-action">Team</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
             <h2>Personal Settings</h2>
-            <div class="col-md-12">
-                <div class="card col-md-4 mb-3">
-                    <div class="card-header">
-                        Personal Settings
-                    </div>
-                    <div class="card-body">
-                        <div class="list-group">
-                            <a href="#" class="list-group-item list-group-item-action">Account details</a>
-                            <a href="#" class="list-group-item list-group-item-action">Email notifications</a>
-                            <a href="#" class="list-group-item list-group-item-action">Contacts</a>
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Personal Settings
+                        </div>
+                        <div class="card-body">
+                            <div class="list-group">
+                                <a href="#" class="list-group-item list-group-item-action">Account details</a>
+                                <a href="#" class="list-group-item list-group-item-action">Email notifications</a>
+                                <a href="#" class="list-group-item list-group-item-action">Contacts</a>
+                            </div>
                         </div>
                     </div>
                 </div>
