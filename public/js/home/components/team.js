147d2648ecbb3e1b5ac5694ce3bdcd3a1a19df7f
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-06 01:30:04 -0400

    Update team.js

--- public/js/home/components/team.js
+++ public/js/home/components/team.js
@@ -3,114 +3,53 @@ var TeamSection = {
     <div class="row member-container">
         <div class="member-section">
             <div class="member-header">Dismac</div>
-            <ul class="member-list">
-                <li>
-                    <span class="email">chirag.p@wagento.com</span>
-                    <span class="permissions">
-                        <span class="badge bg-primary">Set-Up Search</span>
-                        <span class="badge bg-primary">Search Features</span>
-                        <span class="badge bg-primary">View Search</span>
-                        <span class="badge bg-primary">Recommend</span>
-                        <span class="badge bg-secondary">Others</span>
-                    </span>
-                </li>
-                <li>
-                    <span class="email">chsaldana@dismac.com.bo</span>
-                    <span class="permissions">
-                        <span class="badge bg-primary">Set-Up Search</span>
-                        <span class="badge bg-primary">Search Features</span>
-                        <span class="badge bg-primary">View Search</span>
-                        <span class="badge bg-secondary">Others</span>
-                        <span class="badge bg-secondary">General</span>
-                    </span>
-                </li>
-                <li>
-                    <span class="email">crubindecelis@dismac.com.bo</span>
-                    <span class="permissions">
-                        <span class="badge bg-primary">Set-Up Search</span>
-                        <span class="badge bg-primary">Search Features</span>
-                        <span class="badge bg-primary">View Search</span>
-                        <span class="badge bg-secondary">Others</span>
-                    </span>
-                </li>
-                <li>
-                    <span class="email">eflores@dismac.com.bo</span>
-                    <span class="permissions">
-                        <span class="badge bg-primary">Set-Up Search</span>
-                        <span class="badge bg-primary">Search Features</span>
-                        <span class="badge bg-primary">View Search</span>
-                        <span class="badge bg-primary">Recommend</span>
-                        <span class="badge bg-secondary">Others</span>
-                        <span class="badge bg-secondary">General</span>
-                    </span>
-                </li>
-                <li>
-                    <span class="email">ehhuallata@dismac.com.bo (you)</span>
-                    <span class="permissions">
-                        <span class="badge bg-primary">Set-Up Search</span>
-                        <span class="badge bg-primary">Search Features</span>
-                        <span class="badge bg-primary">View Search</span>
-                        <span class="badge bg-primary">Recommend</span>
-                        <span class="badge bg-secondary">Others</span>
-                        <span class="badge bg-secondary">General</span>
-                    </span>
-                </li>
-                <li>
-                    <span class="email">emcostas@dismac.com.bo</span>
-                    <span class="permissions">
-                        <span class="badge bg-primary">Set-Up Search</span>
-                        <span class="badge bg-primary">Search Features</span>
-                        <span class="badge bg-primary">View Search</span>
-                        <span class="badge bg-primary">Recommend</span>
-                        <span class="badge bg-secondary">Others</span>
-                        <span class="badge bg-secondary">General</span>
-                    </span>
-                </li>
-                <li>
-                    <span class="email">masuchet@dismac.com.bo (Owner)</span>
-                    <span class="permissions">
-                        <span class="badge bg-primary">Set-Up Search</span>
-                        <span class="badge bg-primary">Search Features</span>
-                        <span class="badge bg-primary">View Search</span>
-                        <span class="badge bg-primary">Recommend</span>
-                        <span class="badge bg-secondary">Others</span>
-                        <span class="badge bg-secondary">General</span>
-                    </span>
-                </li>
-                <li>
-                    <span class="email">meserrano@dismac.com.bo</span>
-                    <span class="permissions">
-                        <span class="badge bg-primary">Set-Up Search</span>
-                        <span class="badge bg-primary">Search Features</span>
-                        <span class="badge bg-primary">View Search</span>
-                        <span class="badge bg-primary">Recommend</span>
-                        <span class="badge bg-secondary">Others</span>
-                        <span class="badge bg-secondary">General</span>
-                    </span>
-                </li>
-                <li>
-                    <span class="email">mfrojas@dismac.com.bo</span>
-                    <span class="permissions">
-                        <span class="badge bg-primary">Set-Up Search</span>
-                        <span class="badge bg-primary">Search Features</span>
-                        <span class="badge bg-primary">View Search</span>
-                        <span class="badge bg-primary">Recommend</span>
-                        <span class="badge bg-secondary">Others</span>
-                        <span class="badge bg-secondary">General</span>
-                    </span>
-                </li>
-                <li>
-                    <span class="email">mrrivera@dismac.com.bo</span>
-                    <span class="permissions">
-                        <span class="badge bg-primary">Set-Up Search</span>
-                        <span class="badge bg-primary">Search Features</span>
-                        <span class="badge bg-primary">View Search</span>
-                        <span class="badge bg-primary">Recommend</span>
-                        <span class="badge bg-secondary">Others</span>
-                        <span class="badge bg-secondary">General</span>
-                    </span>
-                </li>
-            </ul>
+            <hr>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Member</th>
+                        <th scope="col">Permissions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>chirag.p@wagento.com</td>
+                        <td>
+                            <span class="permissions">
+                                <span class="badge bg-primary">Set-Up Search</span>
+                                <span class="badge bg-primary">Search Features</span>
+                                <span class="badge bg-primary">View Search</span>
+                                <span class="badge bg-primary">Recommend</span>
+                                <span class="badge bg-secondary">Others</span>
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>chsaldana@dismac.com.bo</td>
+                        <td>
+                            <span class="permissions">
+                                <span class="badge bg-primary">Set-Up Search</span>
+                                <span class="badge bg-primary">Search Features</span>
+                                <span class="badge bg-primary">View Search</span>
+                                <span class="badge bg-secondary">Others</span>
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>crubindecelis@dismac.com.bo</td>
+                        <td>
+                            <span class="permissions">
+                                <span class="badge bg-primary">Set-Up Search</span>
+                                <span class="badge bg-primary">Search Features</span>
+                                <span class="badge bg-primary">View Search</span>
+                                <span class="badge bg-primary">Recommend</span>
+                                <span class="badge bg-secondary">Others</span>
+                                <span class="badge bg-secondary">General</span>
+                            </span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
     `,
