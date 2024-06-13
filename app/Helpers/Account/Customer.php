232665f7d4bf6664ed755b8c26c1bfe480930100
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-12 23:35:13 -0400

    Update Customer.php

--- app/Helpers/Account/Customer.php
+++ app/Helpers/Account/Customer.php
@@ -304,7 +304,7 @@ class Customer
         $structure = $this->generateDateArray();
 
         foreach ($structure["label"] as $key => $date) {
-            $structure["data"][] = $collection->where("created_at", "=", $date)->count();
+            $structure["data"][] = $collection->whereRaw('Date(created_at) = '.$date)->count();
         }
 
         return $structure;
@@ -318,7 +318,7 @@ class Customer
         $structure = $this->generateDateArray();
 
         foreach ($structure["label"] as $key => $date) {
-            $structure["data"][] = $collection->where("created_at", "=", $date)->avg("time_execution");
+            $structure["data"][] = $collection->whereRaw('Date(created_at) = '.$date)->avg("time_execution");
         }
 
         return $structure;
