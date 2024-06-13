74ac9aa92fbf6c5dbf01095d84afde8eb9805190
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-13 03:01:16 -0400

    pl-5

--- public/js/home/components/home.js
+++ public/js/home/components/home.js
@@ -98,10 +98,10 @@ var HomeSection = {
                                 <div class="col-2 bd-highlight text-center">Consultas</div>
                                 <div class="col-2 bd-highlight text-center">Registros</div>
                             </div>
-                            <div v-for="data in dataIndex.index" class="col-12 d-flex bd-highlight">
-                                <div class="col-8 bd-highlight">{{data.code}}</div>
-                                <div class="col-2 bd-highlight text-center">{{data.query}}</div>
-                                <div class="col-2 bd-highlight text-center">{{data.record}}</div>
+                            <div v-for="data in dataIndex.index" class="col-12 d-flex">
+                                <div class="col-8 pl-5">{{data.code}}</div>
+                                <div class="col-2 pl-5 text-center">{{data.query}}</div>
+                                <div class="col-2 pl-5 text-center">{{data.record}}</div>
                             </div>
                         </div>
                     </div>
