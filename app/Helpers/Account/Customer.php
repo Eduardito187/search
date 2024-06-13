30679a3e231b3057fc590c26670265235654cf10
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-12 23:51:53 -0400

    Update Customer.php

--- app/Helpers/Account/Customer.php
+++ app/Helpers/Account/Customer.php
@@ -304,7 +304,7 @@ class Customer
         $structure = $this->generateDateArray();
 
         foreach ($structure["label"] as $key => $date) {
-            $structure["data"][] = $collection->where("created_at", "like", "%".$date."%")->count();
+            $structure["data"][] = $collection->where("created_at", "like", "%".$date."%")->count("created_at");
         }
 
         return $structure;
