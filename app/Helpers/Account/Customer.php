34fb36d6edccca88efc296076329d364427bb0eb
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-12 23:24:04 -0400

    Update Customer.php

--- app/Helpers/Account/Customer.php
+++ app/Helpers/Account/Customer.php
@@ -261,8 +261,8 @@ class Customer
         $currentClient = $customer->client;
         //$currentClient->recentMonthHistoryIndex;
         return [
-            "query" => $this->generateStructureDataBody($currentClient->recentMonthHistoryQuerySearch, true),
-            "suggestion" => $this->generateStructureDataBody($currentClient->recentMonthHistoryQuerySearchSuggestion),
+            "query" => $this->generateStructureDataBody($currentClient->recentMonthHistoryQuerySearch(), true),
+            "suggestion" => $this->generateStructureDataBody($currentClient->recentMonthHistoryQuerySearchSuggestion()),
             "data" => []
         ];
     }
@@ -309,7 +309,7 @@ class Customer
         $structure = $this->generateDateArray();
 
         foreach ($structure["label"] as $key => $date) {
-            //$structure["data"][] = $collection->whereDate("created_at", "=", $date)->count() ?? 0;
+            $structure["data"][] = $collection->whereDate("created_at", "=", $date)->count() ?? 0;
         }
 
         return $structure;
@@ -323,7 +323,7 @@ class Customer
         $structure = $this->generateDateArray();
 
         foreach ($structure["label"] as $key => $date) {
-            //$structure["data"][] = $collection->whereDate("created_at", "=", $date)->avg("time_execution") ?? 0;
+            $structure["data"][] = $collection->whereDate("created_at", "=", $date)->avg("time_execution") ?? 0;
         }
 
         return $structure;
