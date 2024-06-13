37e93937ec2e90a6f1974b2d74857f0a88639a20
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-13 19:47:57 -0400

    Update confirmation.blade.php

--- resources/views/mail/confirmation.blade.php
+++ resources/views/mail/confirmation.blade.php
@@ -9,7 +9,7 @@
     <body>
         <div class="container">
             <div class="logo">
-                <img src="<?= $base_url; ?>/img/default-user.png" alt="Logo" class="logo-picture" />
+                <img src="<?= $base_url; ?>/img/picture-logo.png" alt="Logo" class="logo-picture" />
             </div>
             <div class="email-content">
                 <div class="title"><?= $title; ?></div>
