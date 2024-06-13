e8bb33e81a1e6f9cc4fcef25a760a9d073218366
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-13 19:46:46 -0400

    Update confirmation.blade.php

--- resources/views/mail/confirmation.blade.php
+++ resources/views/mail/confirmation.blade.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title; ?></title>
-</head>
-<body>
-    <div class="container">
-        <div class="logo">
-            <img src="<?= $base_url; ?>/img/default-user.png" alt="Logo" class="logo-picture" />
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?= $title; ?></title>
+        <link rel="stylesheet" href="{{ asset('css/reset-password.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <div class="logo">
+                <img src="<?= $base_url; ?>/img/default-user.png" alt="Logo" class="logo-picture" />
+            </div>
+            <div class="email-content">
+                <div class="title"><?= $title; ?></div>
+                <div class="message"><?= $description; ?></div>
+                <div class="message"><?= $footer_text; ?></div>
+            </div>
+            <div class="footer">
+                Copyright © <?= date("Y"); ?> <a href="<?= $base_url; ?>"><?= $app_name; ?></a>. All Rights Reserved.
+            </div>
         </div>
-        <div class="email-content">
-            <div class="title"><?= $title; ?></div>
-            <div class="message"><?= $description; ?></div>
-            <div class="message"><?= $footer_text; ?></div>
-        </div>
-        <div class="footer">
-            Copyright © <?= date("Y"); ?> <a href="<?= $base_url; ?>"><?= $app_name; ?></a>. All Rights Reserved.
-        </div>
-    </div>
-</body>
+    </body>
 </html>
