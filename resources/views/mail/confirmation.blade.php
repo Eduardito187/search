6b54663703d71d888c7c989f39265f75a1498f12
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-13 22:32:07 -0400

    Update confirmation.blade.php

--- resources/views/mail/confirmation.blade.php
+++ resources/views/mail/confirmation.blade.php
@@ -4,7 +4,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title; ?></title>
-        <link rel="stylesheet" href="{{ asset('css/confirm-account.css') }}">
+        <style>
+            @import url('<?= $base_url; ?>/css/confirm-account.css');
+        </style>
     </head>
     <body>
         <div class="container">
