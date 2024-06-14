77c105eed1ed1871af6053846a5939a0bd707ed8
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-13 23:16:52 -0400

    Update restore-password.blade.php

--- resources/views/frontend/account/reset/restore-password.blade.php
+++ resources/views/frontend/account/reset/restore-password.blade.php
@@ -55,11 +55,9 @@
                     </div>
 
                     <div class="form-group row mb-0">
-                        <div class="col-md-12 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Reset Password') }}
-                            </button>
-                        </div>
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Reset Password') }}
+                        </button>
                     </div>
                 </form>
             </div>
