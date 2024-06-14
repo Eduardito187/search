80641eb585e45eadd340eac3a5492b13adfd55fd
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-13 23:36:24 -0400

    Update PasswordResetController.php

--- app/Http/Controllers/Auth/PasswordResetController.php
+++ app/Http/Controllers/Auth/PasswordResetController.php
@@ -23,12 +23,17 @@ class PasswordResetController extends Controller
         $request->validate([
             'token' => 'required',
             'password' => 'required|confirmed',
+            'password_confirmation' => 'required|confirmed'
         ]);
 
         $passwordReset = DB::table('password_resets')->where('token', $request->token)->first();
         
         if (strlen($request->password) < 8) {
             return back()->withErrors(['password' => 'La contraseña no cumple con los parametros de seguridad.']);
+        }
+        
+        if ($request->password != $request->password_confirmation) {
+            return back()->withErrors(['password' => 'La contraseña de confirmación no coinciden.']);
         }
 
         if (!$passwordReset || Carbon::parse($passwordReset->created_at)->addMinutes(10)->isPast()) {
