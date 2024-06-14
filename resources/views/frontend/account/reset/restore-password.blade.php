99b2ee6fd8cf1f456b748e587424a70a37ae04ce
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-13 23:18:05 -0400

    Update restore-password.blade.php

--- resources/views/frontend/account/reset/restore-password.blade.php
+++ resources/views/frontend/account/reset/restore-password.blade.php
@@ -20,23 +20,9 @@
                     <input type="hidden" name="token" value="{{ $token }}">
 
                     <div class="form-group row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
-                        <div class="input-group">
-                            <i class="fa fa-user"></i>
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-                        </div>
-
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-
-                    <div class="form-group row">
                         <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
                         <div class="input-group">
-                            <i class="fa fa-secret"></i>
+                            <i class="fa fa-shield"></i>
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                         </div>
                         @error('password')
@@ -49,7 +35,7 @@
                     <div class="form-group row">
                         <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
                         <div class="input-group">
-                            <i class="fa fa-secret"></i>
+                            <i class="fa fa-shield"></i>
                             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                         </div>
                     </div>
