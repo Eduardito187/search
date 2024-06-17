c21325e27ef292cf288e7c6598594a3b29201e40
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-17 02:05:01 -0400

    Update team.js

--- public/js/home/components/team.js
+++ public/js/home/components/team.js
@@ -12,7 +12,7 @@ var TeamSection = {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
+                    <tr v-for="data in dataPage">
                         <td>chirag.p@wagento.com</td>
                         <td>
                             <span class="permissions">
@@ -24,35 +24,31 @@ var TeamSection = {
                             </span>
                         </td>
                     </tr>
-                    <tr>
-                        <td>chsaldana@dismac.com.bo</td>
-                        <td>
-                            <span class="permissions">
-                                <span class="badge bg-primary">Set-Up Search</span>
-                                <span class="badge bg-primary">Search Features</span>
-                                <span class="badge bg-primary">View Search</span>
-                                <span class="badge bg-secondary">Others</span>
-                            </span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>crubindecelis@dismac.com.bo</td>
-                        <td>
-                            <span class="permissions">
-                                <span class="badge bg-primary">Set-Up Search</span>
-                                <span class="badge bg-primary">Search Features</span>
-                                <span class="badge bg-primary">View Search</span>
-                                <span class="badge bg-primary">Recommend</span>
-                                <span class="badge bg-secondary">Others</span>
-                                <span class="badge bg-secondary">General</span>
-                            </span>
-                        </td>
-                    </tr>
                 </tbody>
             </table>
         </div>
     </div>
     `,
+    data() {
+        return {
+            dataPage: null
+        };
+    },
+    methods: {
+        getTeamsData() {
+            let self = this;
+            window.fetchFontendData('api/account/team-users', 'POST').then(data => {
+                if (data.status && data.code == 200) {
+                    self.dataPage = data.response;
+                }
+            }).catch(error => {
+                console.error('Error en la solicitud:', error);
+            });
+        },
+    },
+    created() {
+        this.getTeamsData();
+    },
     mounted() {
     }
 };
