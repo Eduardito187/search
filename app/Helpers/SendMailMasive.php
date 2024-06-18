b45015544e5d4dc838ea6d268f23dc41e6eeb2eb
Eduard Huallata <ehhuallata@dismac.com.bo>
2024-06-18 11:03:49 -0400

    changes html

--- app/Helpers/SendMailMasive.php
+++ app/Helpers/SendMailMasive.php
@@ -82,7 +82,7 @@ class SendMailMasive
             $mail->isHTML(true);
             $mail->Subject = $this->title;
             $mail->Body = $this->message;
-            Log::info("Body => ".$this->message);
+            $mail->WordWrap = 120;
 
             // Adjuntar archivos (opcional)
             // $mail->addAttachment('/path/to/file');
