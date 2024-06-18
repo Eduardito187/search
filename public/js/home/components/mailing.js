4216e0c6b237bf18fd9d9fa296994ce800aee885
Eduard Huallata <ehhuallata@dismac.com.bo>
2024-06-18 12:59:55 -0400

    changes html

--- public/js/home/components/mailing.js
+++ public/js/home/components/mailing.js
@@ -16,33 +16,15 @@ var MailingSection = {
             </div>
         </div>
         <div class="row mt-4">
-            <div class="col-md-4">
+            <div v-for="data in dataPage" class="col-md-4">
                 <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img :src="data.preview" class="card-img-top" :alt="data.name">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title">{{data.name}}</h5>
+                        <p class="card-text">{{data.description}}</p>
+                        <div v-if="data.indexes.length > 0" class="col-12">
+                            <span v-for="index in data.indexes" class="badge bg-primary">{{index}}</span>
+                        </div>
                     </div>
                 </div>
             </div>
