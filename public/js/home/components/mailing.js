fd58ca4bbe6009d63a7888f96f7eb5ca4e54aa90
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-18 00:03:07 -0400

    Update mailing.js

--- public/js/home/components/mailing.js
+++ public/js/home/components/mailing.js
@@ -1,7 +1,7 @@
 var MailingSection = {
     template: `
     <div class="row">
-        <div class="col-md-12">
+        <div class="row">
             <div class="card">
                 <div class="card-body">
                     <div class="row text-end mt-3">
@@ -15,7 +15,7 @@ var MailingSection = {
                 </div>
             </div>
         </div>
-        <div class="col-md-12">
+        <div class="row mt-4">
             <div class="col-md-4">
                 <div class="card">
                     <img src="..." class="card-img-top" alt="...">
