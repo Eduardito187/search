4eeb04228648d6735995df992c59f7a1f29920ee
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-18 00:39:17 -0400

    Update create-mail.js

--- public/js/home/components/create-mail.js
+++ public/js/home/components/create-mail.js
@@ -57,9 +57,9 @@ var CreateMailSection = {
     mounted() {
 tinymce.init({
   selector: 'textarea#mail-template',
-  plugins: 'preview importcss searchreplace autolink autosave save directionality visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount a11ychecker help formatpainter permanentpen pageembed charmap mentions quickbars emoticons mergetags autocorrect typography advtemplate markdown revisionhistory',
+  plugins: 'preview importcss searchreplace autolink autosave save directionality visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help pageembed charmap mentions quickbars emoticons autocorrect typography',
   mobile: {
-    plugins: 'preview importcss searchreplace autolink autosave save directionality visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount a11ychecker help formatpainter pageembed charmap mentions quickbars emoticons mergetags autocorrect typography advtemplate',
+    plugins: 'preview importcss searchreplace autolink autosave save directionality visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help pageembed charmap mentions quickbars emoticons autocorrect typography',
   },
   menu: {
     tc: {
@@ -68,7 +68,7 @@ tinymce.init({
     }
   },
   menubar: 'file edit view insert format tools table tc help',
-  toolbar: "undo redo | revisionhistory | aidialog aishortcuts | blocks fontsizeinput | bold italic | align numlist bullist | link image | table media pageembed | lineheight  outdent indent | strikethrough forecolor backcolor formatpainter removeformat | charmap emoticons | code fullscreen preview | save print | pagebreak anchor codesample mergetags | addtemplate inserttemplate | addcomment showcomments | ltr rtl | spellcheckdialog a11ycheck", // Note: if a toolbar item requires a plugin, the item will not present in the toolbar if the plugin is not also loaded.
+  toolbar: "undo redo | aidialog aishortcuts | blocks fontsizeinput | bold italic | align numlist bullist | link image | table media pageembed | lineheight  outdent indent | strikethrough forecolor backcolor removeformat | charmap emoticons | code fullscreen preview | save print | pagebreak anchor codesample | addtemplate inserttemplate | addcomment showcomments | ltr rtl | spellcheckdialog a11ycheck", // Note: if a toolbar item requires a plugin, the item will not present in the toolbar if the plugin is not also loaded.
   autosave_ask_before_unload: true,
   autosave_interval: '30s',
   autosave_prefix: '{path}{query}-{id}-',
