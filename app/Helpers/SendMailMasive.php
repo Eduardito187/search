5a951f646ca0725ef9acf24529118f7d68e148fd
Eduard Huallata <ehhuallata@dismac.com.bo>
2024-06-18 08:38:52 -0400

    changes html

--- app/Helpers/SendMailMasive.php
+++ app/Helpers/SendMailMasive.php
@@ -62,7 +62,7 @@ class SendMailMasive
             ini_set('display_errors', 1);
             error_reporting(E_ALL);
             $headers = implode("\r\n", $this->headers);
-
+            \Illuminate\Support\Facades\Log::info($this->message);
             return mail($this->to, $this->title, $this->message, $headers);
         } catch (Exception $e) {
             return false;
