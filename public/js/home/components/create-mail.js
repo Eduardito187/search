4b974c62e702b365fc917706f8e1353ce01e23fe
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-18 02:59:53 -0400

    Update create-mail.js

--- public/js/home/components/create-mail.js
+++ public/js/home/components/create-mail.js
@@ -102,7 +102,7 @@ var CreateMailSection = {
         savedMailing() {
             this.savedTinyMce();
 
-            window.fetchFontendData('mailing/create-masive', 'POST', {
+            window.fetchFontendData('api/mailing/create-masive', 'POST', {
                 name : this.name,
                 description : this.description,
                 mail_template : this.mail_template,
