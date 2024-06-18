0d7ee72f97dbdf12f953287355f5fdd2b08e7fde
Eduard Huallata <ehhuallata@dismac.com.bo>
2024-06-18 14:16:37 -0400

    changes html

--- public/js/home/components/mailing.js
+++ public/js/home/components/mailing.js
@@ -29,6 +29,35 @@ var MailingSection = {
                 </div>
             </div>
         </div>
+        <div class="row mt-2">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row text-end">
+                        <div class="col align-self-end">
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination justify-content-end">
+                                    <li class="page-item disabled">
+                                        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">1</a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">2</a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">3</a>
+                                    </li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">Next</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     `,
     data() {
