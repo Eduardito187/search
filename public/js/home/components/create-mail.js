1c9287f0adc20a62d5addabffc90668ce5140dbb
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-18 00:20:01 -0400

    Update create-mail.js

--- public/js/home/components/create-mail.js
+++ public/js/home/components/create-mail.js
@@ -7,14 +7,14 @@ var CreateMailSection = {
                     <div class="row">
                         <div class="col-md-6">
                             <div class="row p-4">
-                                <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                                <label class="form-label">Nombre</label>
+                                <input type="email" class="form-control" v-model="name">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="row p-4">
-                                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                <label class="form-label">Descripción</label>
+                                <textarea class="form-control" v-model="description" rows="3"></textarea>
                             </div>
                         </div>
                     </div>
