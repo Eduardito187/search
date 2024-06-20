abde053d70ebd4cf7eadf007e211b1fa7d503597
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-20 02:51:59 -0400

    Update mail-sender.js

--- public/js/home/components/mail-sender.js
+++ public/js/home/components/mail-sender.js
@@ -42,11 +42,11 @@ var MailSenderSection = {
                     <div class="accordion accordion-flush" id="accordionFlushExample">
                         <div v-for="data in allCustomersSender" class="accordion-item">
                             <h2 class="accordion-header" :id="data.index_id+'_index'">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="'#'+data.index_id+'_body'" aria-expanded="false" :aria-controls="data.index_id+'_body'">
                                     {{data.index}} ({{data.total_customer}})
                                 </button>
                             </h2>
-                            <div id="flush-collapseOne" class="accordion-collapse collapse" :aria-labelledby="data.index_id+'_index'" data-bs-parent="#accordionFlushExample">
+                            <div :id="data.index_id+'_body'" class="accordion-collapse collapse" :aria-labelledby="data.index_id+'_index'" data-bs-parent="#accordionFlushExample">
                                 <div class="accordion-body">
                                     Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.
                                 </div>
