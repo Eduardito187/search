71ce97a2d98e4c45b1275d5e25b1b8fcd3a581b7
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-24 02:03:04 -0400

    option-menu

--- public/js/home/components/data.js
+++ public/js/home/components/data.js
@@ -3,9 +3,9 @@ var DataSection = {
     <div class="row">
         <div class="col-md-3">
             <ul class="list-group">
-                <li class="list-group-item">Eventos</li>
-                <li class="list-group-item">Conectores</li>
-                <li class="list-group-item">Indices</li>
+                <li class="list-group-item item-selected"><div class="option-menu"></div>Eventos</li>
+                <li class="list-group-item item-selected"><div class="option-menu"></div>Conectores</li>
+                <li class="list-group-item item-selected"><div class="option-menu"></div>Indices</li>
             </ul>
         </div>
         <div class="col-md-9"></div>
