4ab3afff8b0a6f0a8e75c7f0d521f606fb370f1a
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-24 01:59:05 -0400

    line

--- public/js/home/components/data.js
+++ public/js/home/components/data.js
@@ -1,35 +1,14 @@
 var DataSection = {
     template: `
     <div class="row">
-        <div class="col-md-2">
-            <div class="timeline">
-                <div class="timeline-item">
-                    <div class="timeline-icon blue">
-                        <i class="fa fa-user"></i>
-                    </div>
-                    <div class="timeline-content">
-                        <h3>Required Customer Survey</h3>
-                    </div>
-                </div>
-                <div class="timeline-item">
-                    <div class="timeline-icon green">
-                        <i class="fa fa-bullseye"></i>
-                    </div>
-                    <div class="timeline-content">
-                        <h3>Create a Product Launching Strategy</h3>
-                    </div>
-                </div>
-                <div class="timeline-item">
-                    <div class="timeline-icon orange">
-                        <i class="fa fa-users"></i>
-                    </div>
-                    <div class="timeline-content">
-                        <h3>Identify Beta Testers</h3>
-                    </div>
-                </div>
-            </div>
+        <div class="col-md-3">
+            <ul class="list-group">
+                <li class="list-group-item">Eventos</li>
+                <li class="list-group-item">Conectores</li>
+                <li class="list-group-item">Indices</li>
+            </ul>
         </div>
-        <div class="col-md-10"></div>
+        <div class="col-md-9"></div>
     </div>
     `
 };
