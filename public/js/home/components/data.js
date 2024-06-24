84b59a6e5239755528fcce5905012aaa653efac5
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-24 02:09:56 -0400

    menu-active

--- public/js/home/components/data.js
+++ public/js/home/components/data.js
@@ -3,12 +3,33 @@ var DataSection = {
     <div class="row">
         <div class="col-md-3">
             <ul class="list-group">
-                <li class="list-group-item item-selected"><div class="option-menu"></div>Eventos</li>
-                <li class="list-group-item item-selected"><div class="option-menu"></div>Conectores</li>
-                <li class="list-group-item item-selected"><div class="option-menu"></div>Indices</li>
+                <li class="list-group-item item-selected" @click="selectedMenu('events')">
+                    <div :class="'option-menu '+(activeSection == 'events' ? 'menu-active' : '')"></div>
+                    Eventos
+                </li>
+                <li class="list-group-item item-selected" @click="selectedMenu('connectors')">
+                    <div :class="'option-menu '+(activeSection == 'connectors' ? 'menu-active' : '')"></div>
+                    Conectores
+                </li>
+                <li class="list-group-item item-selected" @click="selectedMenu('index')">
+                    <div :class="'option-menu '+(activeSection == 'index' ? 'menu-active' : '')"></div>
+                    Indices
+                </li>
             </ul>
         </div>
         <div class="col-md-9"></div>
     </div>
-    `
+    `,
+    data() {
+        return {
+            activeSection: '',
+        };
+    },
+    methods: {
+        selectedMenu(option) {
+            this.activeSection = option;
+        }
+    },
+    created() {
+    }
 };
