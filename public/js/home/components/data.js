6580ea6ccea94378b020c076d0e9f1153cb3d0a1
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-24 01:55:12 -0400

    time

--- public/js/home/components/data.js
+++ public/js/home/components/data.js
@@ -5,32 +5,26 @@ var DataSection = {
             <div class="timeline">
                 <div class="timeline-item">
                     <div class="timeline-icon blue">
-                        <i class="fas fa-user"></i>
+                        <i class="fa fa-user"></i>
                     </div>
                     <div class="timeline-content">
-                        <span class="time">8 hours ago</span>
                         <h3>Required Customer Survey</h3>
-                        <p>Get advice and opinion from customers by asking valuable survey questions using paid data collection tools and plugins.</p>
                     </div>
                 </div>
                 <div class="timeline-item">
                     <div class="timeline-icon green">
-                        <i class="fas fa-bullseye"></i>
+                        <i class="fa fa-bullseye"></i>
                     </div>
                     <div class="timeline-content">
-                        <span class="time">6 hours ago</span>
                         <h3>Create a Product Launching Strategy</h3>
-                        <p>Define your product launching goals, and prepare materials that can help us later as we let others know about our product and its features.</p>
                     </div>
                 </div>
                 <div class="timeline-item">
                     <div class="timeline-icon orange">
-                        <i class="fas fa-users"></i>
+                        <i class="fa fa-users"></i>
                     </div>
                     <div class="timeline-content">
-                        <span class="time">3 hours ago</span>
                         <h3>Identify Beta Testers</h3>
-                        <p>Find early adopters, and loyal customers base to take feedback about the newly created product and finalize the launch content strategy.</p>
                     </div>
                 </div>
             </div>
