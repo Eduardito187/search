c6d1ea854174317114b4f4e5293ceff33cb61b3e
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-24 01:51:26 -0400

    time-line

--- public/js/home/components/analitycs.js
+++ public/js/home/components/analitycs.js
@@ -1,79 +1,41 @@
 var AnalitycsSection = {
     template: `
-    
     <div class="row">
-        <div class="row">
-            <div class="card">
-                <div class="card-body">
-                    <div class="row text-end">
-                        <div class="col align-self-end">
-                            <button type="button" class="btn-save-eduard-search" @click="createMail()">
-                                <span>Create Mail</span>
-                                <i class="fa fa-plus"></i>
-                            </button>
-                        </div>
+        <div class="col-md-2">
+            <div class="timeline">
+                <div class="timeline-item">
+                    <div class="timeline-icon blue">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="timeline-content">
+                        <span class="time">8 hours ago</span>
+                        <h3>Required Customer Survey</h3>
+                        <p>Get advice and opinion from customers by asking valuable survey questions using paid data collection tools and plugins.</p>
+                    </div>
+                </div>
+                <div class="timeline-item">
+                    <div class="timeline-icon green">
+                        <i class="fas fa-bullseye"></i>
+                    </div>
+                    <div class="timeline-content">
+                        <span class="time">6 hours ago</span>
+                        <h3>Create a Product Launching Strategy</h3>
+                        <p>Define your product launching goals, and prepare materials that can help us later as we let others know about our product and its features.</p>
+                    </div>
+                </div>
+                <div class="timeline-item">
+                    <div class="timeline-icon orange">
+                        <i class="fas fa-users"></i>
+                    </div>
+                    <div class="timeline-content">
+                        <span class="time">3 hours ago</span>
+                        <h3>Identify Beta Testers</h3>
+                        <p>Find early adopters, and loyal customers base to take feedback about the newly created product and finalize the launch content strategy.</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row mt-4">
-            <div class="row text-end">
-                <p>{{total}} items</p>
-            </div>
-        </div>
-        <div class="row">
-            <div v-for="data in dataPage" class="col-md-4 mb-2" @click="selectedMail(data.id)">
-                <div class="card">
-                    <img :src="data.preview" class="card-img-top picture-preview-mail" :alt="data.name">
-                    <div class="card-body">
-                        <h5 class="card-title">{{data.name}}</h5>
-                        <p class="card-text">{{data.description}}</p>
-                        <div v-if="data.indexes.length > 0" class="col-12">
-                            <span v-for="index in data.indexes" class="badge bg-primary">{{index}}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row mt-2">
-            <div class="card">
-                <div class="card-body">
-                    <div class="row text-end">
-                        <div class="col align-self-end">
-                            <nav aria-label="Page navigation example">
-                                <ul class="pagination justify-content-end">
-                                    <li v-if="current_page > 1" class="page-item" @click="selectedPage(current_page-1)">
-                                        <span class="page-link" tabindex="-1">
-                                            <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                                        </span>
-                                    </li>
-                                    <li v-if="(current_page-2) > 0" class="page-item" @click="selectedPage(current_page-2)">
-                                        <span class="page-link">{{current_page-2}}</span>
-                                    </li>
-                                    <li v-if="(current_page-1) > 0" class="page-item" @click="selectedPage(current_page-1)">
-                                        <span class="page-link">{{current_page-1}}</span>
-                                    </li>
-                                    <li class="page-item" @click="selectedPage(current_page)">
-                                        <span class="page-link active-page">{{current_page}}</span>
-                                    </li>
-                                    <li v-if="(current_page+1) <= last_page" class="page-item" @click="selectedPage(current_page+1)">
-                                        <span class="page-link">{{current_page+1}}</span>
-                                    </li>
-                                    <li v-if="(current_page+2) <= last_page" class="page-item" @click="selectedPage(current_page+2)">
-                                        <span class="page-link">{{current_page+2}}</span>
-                                    </li>
-                                    <li v-if="current_page != last_page" class="page-item" @click="selectedPage(current_page+1)">
-                                        <span class="page-link">
-                                            <i class="fa fa-chevron-right" aria-hidden="true"></i>
-                                        </span>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div class="col-md-10"></div>
     </div>
     `,
     data() {
