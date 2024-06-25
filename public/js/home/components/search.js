f4185c5f10913edb2f3eb7230626c7093044b808
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-25 01:06:29 -0400

    Update search.js

--- public/js/home/components/search.js
+++ public/js/home/components/search.js
@@ -1,3 +1,59 @@
 var SearchSection = {
-    template: '<div>Hola, soy un users!</div>'
+    template: `
+    <div class="row">
+        <div class="col-md-2">
+            <nav-col :items="colNav"></nav-col>
+        </div>
+        <div class="col-md-10">
+            <div class="container mt-4">
+                <h1>Eventos</h1>
+                <div class="row mt-2">
+                    <nav-header :items="headerNav"></nav-header>
+                </div>
+                <div class="row mt-2"></div>
+            </div>
+            <div class="container mt-4">
+                <h1>Conectores</h1>
+                <div class="row mt-2">
+                    <nav-header :items="headerNavV2"></nav-header>
+                </div>
+                <div class="row mt-2"></div>
+            </div>
+            <div class="container mt-4">
+                <h1>Indices</h1>
+                <div class="row mt-2">
+                    <nav-header :items="headerNavV2"></nav-header>
+                </div>
+                <div class="row mt-2"></div>
+            </div>
+        </div>
+    </div>
+    `,
+    data() {
+        return {
+            activeSection: 'events',
+            colNav: [
+                {key: "events", label: "Eventos"},
+                {key: "connectors", label: "Conectores"},
+                {key: "index", label: "Indices"}
+            ],
+            headerNav: [
+                {key: "event-base", label: "Eventos base"},
+                {key: "debug", label: "Depurador"},
+                {key: "connector", label: "Conectores"},
+                {key: "setting", label: "Ajustes"}
+            ],
+            headerNavV2: [
+                {key: "home", label: "Home"},
+                {key: "tasks", label: "Tasks"},
+                {key: "sources", label: "Sources"},
+                {key: "destinations", label: "Destinations"}
+            ]
+        };
+    },
+    methods: {
+        selectedMenu(option) {
+            this.activeSection = option;
+        }
+    }
 };
