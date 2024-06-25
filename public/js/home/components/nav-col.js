fb774fb9975d094a1d0024edbaf9cbc5232e628e
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-25 00:35:10 -0400

    Update nav-col.js

--- public/js/home/components/nav-col.js
+++ public/js/home/components/nav-col.js
@@ -1,7 +1,7 @@
 Vue.component('nav-col', {
     template: `
     <ul class="list-group">
-        <li v-for="item in items" class="list-group-item item-selected" @click="selectedMenu(item.key)">
+        <li v-for="item in items" class="item list-group-item item-selected" @click="selectedMenu(item.key)">
             <div :class="'option-menu '+(activeSection == item.key ? 'menu-active' : '')"></div>
             <small>{{item.label}}</small>
         </li>
