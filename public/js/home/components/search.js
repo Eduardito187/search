99898e237a72cfebd5bf6e5f1bbd47a6b4265be1
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-06-27 00:36:40 -0400

    Update search.js

--- public/js/home/components/search.js
+++ public/js/home/components/search.js
@@ -2,9 +2,30 @@ var SearchSection = {
     template: `
     <div class="row">
         <div class="col-md-2">
-            <nav-col :items="colNav"></nav-col>
-            <br>
-            <nav-col :items="colNav"></nav-col>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="configuration-nav">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#configuration-nav-body" aria-expanded="false" aria-controls="configuration-nav-body">
+                        Configuracion
+                    </button>
+                </h2>
+                <div id="configuration-nav-body" class="accordion-collapse collapse" aria-labelledby="configuration-nav" data-bs-parent="#configuration-nav">
+                    <div class="accordion-body">
+                        <nav-col :items="colNav"></nav-col>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item mt-2">
+                <h2 class="accordion-header" id="event-nav">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#event-nav-body" aria-expanded="false" aria-controls="event-nav-body">
+                        Configuracion
+                    </button>
+                </h2>
+                <div id="event-nav-body" class="accordion-collapse collapse" aria-labelledby="event-nav" data-bs-parent="#event-nav">
+                    <div class="accordion-body">
+                        <nav-col :items="colNav"></nav-col>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="col-md-10">
             <div class="container mt-4">
