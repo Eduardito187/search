fadcbf78beeaf12c94745007a0da67ec38b59db8
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-07-04 00:05:17 -0400

    Update dashboard.js

--- public/js/home/components/dashboard.js
+++ public/js/home/components/dashboard.js
@@ -28,10 +28,10 @@ var DashboardSection = {
             var options = {
                 series: [{
                     name: 'series1',
-                    data: [31, 40, 28, 51, 42, 109, 100]
+                    data: [31, 40, 28, 51, 42, 109, 100, 28, 51, 42, 109, 100]
                 }, {
                     name: 'series2',
-                    data: [11, 32, 45, 32, 34, 52, 41]
+                    data: [11, 32, 45, 32, 34, 52, 41, 45, 32, 34, 52, 41]
                 }],
                 chart: {
                     height: 350,
