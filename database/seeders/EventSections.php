5252488a3edfbd27b7e9946cb006683d42e0b474
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-07-03 23:22:57 -0400

    Update EventSections.php

--- database/seeders/EventSections.php
+++ database/seeders/EventSections.php
@@ -17,21 +17,21 @@ class EventSections extends Seeder
     public function run()
     {
         if (EventSection::count() == 0) {
-            DB::table("sorting_type")->insert([
+            DB::table("event_section")->insert([
                 "id" => 1,
                 "name" => "SEARCH",
                 "code" => "search",
                 "created_at" => date("Y-m-d H:i:s"),
                 "updated_at" => null
             ]);
-            DB::table("sorting_type")->insert([
+            DB::table("event_section")->insert([
                 "id" => 2,
                 "name" => "RECOMMEND",
                 "code" => "recommend",
                 "created_at" => date("Y-m-d H:i:s"),
                 "updated_at" => null
             ]);
-            DB::table("sorting_type")->insert([
+            DB::table("event_section")->insert([
                 "id" => 3,
                 "name" => "CUSTOM",
                 "code" => "custom",
