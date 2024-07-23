a682b4695004fee58b71860fefe04bcd81cc705c
Eduardito187 <63282723+Eduardito187@users.noreply.github.com>
2024-07-22 23:04:21 -0400

    Update key.js

--- public/js/home/components/key.js
+++ public/js/home/components/key.js
@@ -13,19 +13,24 @@ var KeySection = {
             <div class="api-key-header">Application token</div>
             <div class="api-key-value">{{dataPage.client_token}}</div>
         </div>
-        <div class="api-key-section p3">
-            <div class="api-key-section">
-                <div class="api-key-header">Write API Key</div>
-                <div class="api-key-value">
-                    <span>••••••••••••••••••••••••••••••••••••••</span>
-                    <button class="btn btn-warning btn-sm">Regenerate</button>
+        <div v-if="dataPage.index != null" class="api-key-section p3">
+            <div v-for="data in dataPage.index" class="api-key-section p3">
+                <div v-if="data.name != null" class="api-key-section">
+                    <div class="api-key-header">Index name</div>
+                    <div class="api-key-value">{{data.name}}</div>
                 </div>
-            </div>
-            <div class="api-key-section">
-                <div class="api-key-header">Admin API Key</div>
-                <div class="api-key-value">
-                    <span>••••••••••••••••••••••••••••••••••••••</span>
-                    <button class="btn btn-warning btn-sm">Regenerate</button>
+                <div v-if="data.code != null" class="api-key-section">
+                    <div class="api-key-header">Index code</div>
+                    <div class="api-key-value">{{data.code}}</div>
+                </div>
+                <div class="api-key-section">
+                    <div class="api-key-header">Write API Key</div>
+                    <div class="api-key-value">
+                        <button class="btn btn-warning btn-sm">Show</button>
+                        <input type="hidden" :id="data.code" :value="data.token" />
+                        <span>••••••••••••••••••••••••••••••••••••••</span>
+                        <button class="btn btn-warning btn-sm">Regenerate</button>
+                    </div>
                 </div>
             </div>
         </div>
